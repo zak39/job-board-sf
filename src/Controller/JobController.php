8651cf988fae4ccf465291567db0964cc6fb4271
zak39 <fotia.baptiste@hotmail.com>
2024-10-01 22:30:40 +0200

    feat(twig): Render an offre in show template

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Offre;
 use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,14 @@ class JobController extends AbstractController
 
         return $this->render('job/index.html.twig', [
             'offres' => $offres,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_show')]
+    public function show(Offre $offre): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'offre' => $offre,
         ]);
     }
 }
