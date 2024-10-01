7af39f1a9985c465170539ca4595ecb2775041d6
zak39 <fotia.baptiste@hotmail.com>
2024-10-01 22:05:46 +0200

    feat(controller): Render offres in Twig template

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class JobController extends AbstractController
 {
     #[Route('/', name: 'app_job')]
-    public function index(): Response
+    public function index(OffreRepository $offreRepository): Response
     {
+        $offres = $offreRepository->findAll();
+
         return $this->render('job/index.html.twig', [
-            'controller_name' => 'JobController',
+            'offres' => $offres,
         ]);
     }
 }
