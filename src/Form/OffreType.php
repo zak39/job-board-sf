4f3b8d43bc9c6954af82fd121e6ef238f0678295
zak39 <fotia.baptiste@hotmail.com>
2024-12-20 19:48:31 +0100

    feat(form): The salary field is a MoneyType

--- src/Form/OffreType.php
+++ src/Form/OffreType.php
@@ -7,6 +7,7 @@ use App\Entity\Offre;
 use App\Entity\Tag;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -32,10 +33,12 @@ class OffreType extends AbstractType
                     'row' => '3'
                 ],
             ])
-            ->add('salaire', TextType::class, [
+            ->add('salaire', MoneyType::class, [
                 'label' => 'Salaire de l\'offre',
                 'attr' => [
-                    'class' => 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                    'class' => 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                    'maxlength' => 7,
+                    'minlength' => 0,
                 ],
             ])
             ->add('Entreprise', EntityType::class, [
